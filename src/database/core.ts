6ed48cefb04784f6394f303df277f7985c6e7813
Omar Ezzat <omar.ezzat@protonmail.com>
2020-12-25 15:50:58 +0100

    add db core

---
+++ src/database/core.ts
@@ -0,0 +1,44 @@
+import {Pool, PoolClient} from "pg";
+
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+// Uses the env vars to initialize database connection
+const pool = new Pool();
+
+module.exports = {
+  async query(text: string, params:any) {
+    const start = Date.now()
+    const res = await pool.query(text, params)
+    const duration = Date.now() - start
+    console.log('executed query', { text, duration, rows: res.rowCount })
+    return res
+  },
+  async getClient() {
+    const client: PoolClient & {lastQuery?: any[]} = await pool.connect();
+    const query = client.query;
+    const release = client.release;
+    // set a timeout of 5 seconds, after which we will log this client's last query
+    const timeout = setTimeout(() => {
+      console.error('A client has been checked out for more than 5 seconds!')
+      console.error(`The last executed query on this client was: ${client.lastQuery}`)
+    }, 5000);
+    // monkey patch the query method to keep track of the last query executed
+    // @ts-ignore
+    client.query = (...args:any[]) => {
+      client.lastQuery = args
+      // @ts-ignore
+      return query.apply(client, args)
+    }
+    client.release = () => {
+      // clear our timeout
+      clearTimeout(timeout)
+      // set the methods back to their old un-monkey-patched version
+      client.query = query
+      client.release = release
+      return release.apply(client)
+    }
+    return client
+  }
+}
