77718230cf54fc10db3d905f8af3540f26270128
Omar Ezzat <omar.ezzat@protonmail.com>
2020-12-29 13:29:37 +0100

    change timestamp to timestampz and add new roles

--- docker/docker-entrypoint-initdb.d/init.sql
+++ docker/docker-entrypoint-initdb.d/init.sql
@@ -1,7 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS sidekick;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-CREATE ROLE sidekick_api;
+CREATE ROLE sidekick_api NOINHERIT;
+CREATE ROLE sidekick_public NOINHERIT;
+CREATE ROLE sidekick_user NOINHERIT;
+
+GRANT sidekick_public TO sidekick_api;
+GRANT sidekick_user TO sidekick_api;
 
 SET SCHEMA 'sidekick';
 
