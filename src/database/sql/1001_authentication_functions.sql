f0d3d2185bb509697f0bd3ebd4ccea04e7d19664
Omar Ezzat <omar.ezzat@protonmail.com>
2020-12-30 17:52:42 +0100

    add new auth function via username email in psql

--- src/database/sql/1001_authentication_functions.sql
+++ src/database/sql/1001_authentication_functions.sql
@@ -1,3 +1,4 @@
+-- FUNCTION TO AUTHENTICATE WITH EMAIL AND PASSWORD
 create or replace function sidekick.authenticate_user_by_email_password (
   email text,
   password text
@@ -20,3 +21,27 @@ $$ language plpgsql strict security definer;
 
 comment on function sidekick.authenticate_user_by_email_password(text, text) is 'Creates a JWT token that will securely identify an user and give them certain permissions. This token expires in 2 days.';
 grant execute on function sidekick.authenticate_user_by_email_password(text, text) to sidekick_public, sidekick_user;
+
+-- FUNCTION TO AUTHENTICATE WITH USERNAME AND PASSWORD
+create or replace function sidekick.authenticate_user_by_username_password (
+  username text,
+  password text
+) returns sidekick.jwt_token as $$
+declare
+  auth_user sidekick_private.users_password;
+begin
+  select up.* into auth_user
+  from sidekick.users_decoration ud
+  inner join sidekick_private.users_password up on ud.id = up.id
+  where ud.username = $1 AND ud.username is not null;
+
+  if auth_user.password_hash = crypt(password, auth_user.password_hash) then
+    return ('sidekick_user', auth_user.id, extract(epoch from (now() + interval '2 days')))::sidekick.jwt_token;
+  else
+    return null;
+  end if;
+end;
+$$ language plpgsql strict security definer;
+
+comment on function sidekick.authenticate_user_by_username_password(text, text) is 'Creates a JWT token that will securely identify an user and give them certain permissions. This token expires in 2 days.';
+grant execute on function sidekick.authenticate_user_by_username_password(text, text) to sidekick_public, sidekick_user;
