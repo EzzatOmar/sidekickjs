b02fee9cf3e35c7882832fb63e1198beb66ddcb6
Omar Ezzat <omar.ezzat@protonmail.com>
2021-02-04 22:56:01 +0100

    reduce rate-limiter

--- src/middleware/rate-limiter.ts
+++ src/middleware/rate-limiter.ts
@@ -4,7 +4,7 @@ import { RateLimiterMemory, IRateLimiterRes } from "rate-limiter-flexible";
 // means that the user-agent has 200 points every 2 seconds
 const rateLimiter = new RateLimiterMemory({
   points: 200,
-  duration: 2, // Store number for two second
+  duration: 1, // Store number for two second
   blockDuration: 60, // Block for 1 minute
 });
 
