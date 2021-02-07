9bbb5c0e1db05047a4f8d0cb7c00d309f605c9dd
Omar Ezzat <omar.ezzat@protonmail.com>
2021-02-07 17:03:11 +0100

    add basic custom example

---
+++ custom/tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  purge: [
+    './dist/pages/**/*.html',
+    './dist/pages/**/*.htm',
+    './dist/pages/**/*.handlebars',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    }
+  },
+  plugins: [
+  ],
+  variants: {
+    extend: {},
+  },
+}
