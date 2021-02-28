3f45c7eef7ea7ee9f0998401e7258bd5904125b2
Omar Ezzat <omar.ezzat@protonmail.com>
2021-02-28 11:05:34 +0100

    remove log

--- src/database/core.ts
+++ src/database/core.ts
@@ -7,7 +7,7 @@ export async function query(text: string, params:any = []):Promise<QueryResult> 
   const start = Date.now();
   const res = await pool.query(text, params);
   const duration = Date.now() - start;
-  console.log('executed query', { text, duration, rows: res.rowCount });
+  // console.log('executed query', { text, duration, rows: res.rowCount });
   return res
 }
 
