d504ee20e798c46fb127e395e000b283167e214b
Omar Ezzat <omar.ezzat@protonmail.com>
2021-05-02 12:48:13 +0200

    add new handlebars helper fn local to create new dynamic vars

--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,8 @@
 import { readFileSync, ReadStream, existsSync } from "fs";
 import Handlebars_ from "handlebars";
 // import Hbs from "handlebars";
-import {getFileFromDir} from "./utils/files";
-import {ParameterizedContext, Next} from "koa";
+import { getFileFromDir } from "./utils/files";
+import { ParameterizedContext, Next } from "koa";
 import { syncQueryReadOnly } from "./database/sidekick_api";
 // var promisedHandlebars = require('promised-handlebars')
 // const Handlebars = promisedHandlebars(Handlebars_)
@@ -10,11 +10,11 @@ import { syncQueryReadOnly } from "./database/sidekick_api";
 
 const CustomHandlebars = Handlebars_.create();
 
-function getPartialDirectory(){
-  if(existsSync("./custom/resources/public/web/partials/handlebars")) {
+function getPartialDirectory() {
+  if (existsSync("./custom/resources/public/web/partials/handlebars")) {
     return [6, "./custom/resources/public/web/partials/handlebars"];
   }
-  else if(existsSync("./custom/dist/pages")) {
+  else if (existsSync("./custom/dist/pages")) {
     return [3, "./custom/dist/pages"];
   }
   else {
@@ -24,24 +24,24 @@ function getPartialDirectory(){
 
 export function registerCustomPartials() {
   let partialDir = getPartialDirectory();
-  if(partialDir) {
+  if (partialDir) {
     // @ts-ignore
     getFileFromDir(partialDir[1], [], '\.(handlebars|html|htm)')
-    .map((x:string):[string, string] => {
-       // @ts-ignore
-      let arr = x.split('/').splice(partialDir[0]);
-      arr[arr.length - 1] = arr[arr.length - 1].split('.')[0];
-      let ret;
-      if(arr[arr.length - 1] === 'index') {
-        ret = arr.slice(0, -1).join('.');
-      } else {
-        ret = arr.join('.');
-      }
-      return [ret, './' + x];
-    })
-    .map( ([partial_name, path]) => {
-      CustomHandlebars.registerPartial(partial_name, readFileSync(path, "utf-8"))
-    });
+      .map((x: string): [string, string] => {
+        // @ts-ignore
+        let arr = x.split('/').splice(partialDir[0]);
+        arr[arr.length - 1] = arr[arr.length - 1].split('.')[0];
+        let ret;
+        if (arr[arr.length - 1] === 'index') {
+          ret = arr.slice(0, -1).join('.');
+        } else {
+          ret = arr.join('.');
+        }
+        return [ret, './' + x];
+      })
+      .map(([partial_name, path]) => {
+        CustomHandlebars.registerPartial(partial_name, readFileSync(path, "utf-8"))
+      });
   }
 }
 
@@ -53,34 +53,47 @@ export function registerCustomHelpers() {
     try {
       let ret = "";
       let r = syncQueryReadOnly(jwt, stmt, [])
-      for(const context of r)
+      for (const context of r)
         ret = ret + options.fn(context);
       return ret;
     } catch (err) {
       console.log(stmt, err);
-      return "BAD";
+      return "BAD <br/>" + err.message;
     }
+  });
 
-})
+  CustomHandlebars.registerHelper('local', function (context, options) {
+    try {
+      let addon:any = {};
+      for(let k in options.hash) {
+        addon[k] = CustomHandlebars.compile(options.hash[k])(context);
+      }
+      let new_context = Object.assign(context, addon);
+      return options.fn(new_context);
+    } catch(err) {
+      console.log(context, err);
+      return "BAD <br/>" + err.message;
+    }
+  });
 }
 registerCustomHelpers();
 
 
-function readStream(stream:ReadStream, encoding:BufferEncoding = "utf8"):Promise<string> {
+function readStream(stream: ReadStream, encoding: BufferEncoding = "utf8"): Promise<string> {
   stream.setEncoding(encoding);
   return new Promise((resolve, reject) => {
-      let data = "";
-      stream.on("data", chunk => data += chunk);
-      stream.on("end", () => resolve(data));
-      stream.on("error", error => reject(error));
+    let data = "";
+    stream.on("data", chunk => data += chunk);
+    stream.on("end", () => resolve(data));
+    stream.on("error", error => reject(error));
   });
 }
 
-export function compile_handlebars(html:string, view: any){
+export function compile_handlebars(html: string, view: any) {
   return CustomHandlebars.compile(html)(view);
 }
 
-export function render_html(path: string, view: any):string{
+export function render_html(path: string, view: any): string {
   return compile_handlebars(readFileSync(path, "utf-8"), view);
 }
 
@@ -90,7 +103,7 @@ export async function mw_render_html(ctx: ParameterizedContext, next: Next) {
   let is_html = (!!ct && ctx.response.body && ct.includes('text/html'));
   let is_handlebars = ctx.url.endsWith(".handlebars");
   let no_redirect = ctx.status < 300 || 399 < ctx.status;
-  if( no_redirect && (is_html || is_handlebars) && ctx.response.body ){
+  if (no_redirect && (is_html || is_handlebars) && ctx.response.body) {
     let html = await readStream(ctx.response.body);
     // NOTE: handlebars vars are resolved here
     let rendered = compile_handlebars(html, {
@@ -100,7 +113,7 @@ export async function mw_render_html(ctx: ParameterizedContext, next: Next) {
       local: (process.env.ENVIRONMENT as string).toLowerCase() === 'local'
     });
     ctx.response.body = rendered;
-    if(ctx.url.endsWith(".handlebars")) ctx.response.set('Content-Type', 'text/plain');
+    if (ctx.url.endsWith(".handlebars")) ctx.response.set('Content-Type', 'text/plain');
 
   }
 }
